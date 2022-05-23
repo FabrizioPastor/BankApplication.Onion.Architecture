bf8766c063950558f94c80690dbf3c9437ef012b
fabriziopastor <fabriziopastors@outlook.com>
2022-05-22 21:32:02 -0500

    Add Constructors for ApiExceptions.cs class

--- Application/Exceptions/ApiExceptions.cs
+++ Application/Exceptions/ApiExceptions.cs
@@ -1,6 +1,19 @@
+using System.Globalization;
+
 namespace Application.Exceptions;
 
 public class ApiExceptions : Exception
 {
-    
+    public ApiExceptions() : base()
+    {
+    }
+
+    public ApiExceptions(string? message) : base(message)
+    {
+    }
+
+    public ApiExceptions(string? message, object[] args) : 
+        base(string.Format(CultureInfo.CurrentCulture, message!, args))
+    {
+    }
 }
