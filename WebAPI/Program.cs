6d619e523d9a43cc120166c785e2ea3a8cd30746
fabriziopastor <fabriziopastors@outlook.com>
2022-05-22 21:49:36 -0500

    Using UseErrorHandlingMiddleWare extension method

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Application;
 using Persistence;
+using WebAPI.Extnesions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseErrorHalndlingMiddleWare();
 
 app.UseHttpsRedirection();
 
