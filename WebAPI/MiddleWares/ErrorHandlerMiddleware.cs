2435b6b7f64d81eda09be47eef912f97cd653d5f
fabriziopastor <fabriziopastors@outlook.com>
2022-05-22 21:48:19 -0500

    Custom error handling implementation

--- WebAPI/MiddleWares/ErrorHandlerMiddleware.cs
+++ WebAPI/MiddleWares/ErrorHandlerMiddleware.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Text.Json;
+using Application.Wrappers;
+
 namespace WebAPI.MiddleWares;
 
 public class ErrorHandlerMiddleware
@@ -9,20 +13,41 @@ public class ErrorHandlerMiddleware
         _next = next;
     }
 
-    public async Task Invoke(HttpContext content)
+    public async Task Invoke(HttpContext context)
     {
         try
         {
-            await _next(content);
+            await _next(context);
         }
         catch (Exception error)
         {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            var responseModel = new Response<string>() {Succeeded = false, Message = error?.Message};
+            
             switch (error)
             {
-                case Application.Exceptions.ApiExceptions:
-                    
+                case Application.Exceptions.ApiExceptions e:
+                    //custom application error (API)
+                    response.StatusCode = (int) HttpStatusCode.BadRequest;
+                    break;
+                case Application.Exceptions.ValidationException e:
+                    //custom application error
+                    response.StatusCode = (int) HttpStatusCode.BadRequest;
+                    responseModel.Errors = e.Errors;
+                    break;
+                case KeyNotFoundException e:
+                    //Not found error
+                    response.StatusCode = (int) HttpStatusCode.NotFound;
+                    break;
+                default:
+                    //unhandled error
+                    response.StatusCode = (int) HttpStatusCode.InternalServerError;
                     break;
             }
+
+            var result = JsonSerializer.Serialize(responseModel);
+            await response.WriteAsync(result);
         }
     }
 }
